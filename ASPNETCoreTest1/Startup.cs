4ae71be8451c2c5e674281503ed46601ee954c3b
Petr Plodík <plodik@outlook.com>
2019-12-30 15:09:15 +0100

    Added AppInsights to project

--- ASPNETCoreTest1/Startup.cs
+++ ASPNETCoreTest1/Startup.cs
@@ -24,6 +24,7 @@ namespace ASPNETCoreTest1
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddApplicationInsightsTelemetry();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
