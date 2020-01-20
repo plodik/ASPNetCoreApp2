0758cd9657add7fc6dc6c27f3e203b1bbb0a757d
Petr Plodík <plodik@outlook.com>
2020-01-20 20:42:10 +0100

    Unit test project added with 3 tests

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,26 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.AreEqual(1, 1);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
