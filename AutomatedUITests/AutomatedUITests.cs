8a981ab2fa4f33da00d44c4174c54d2694dd17d1
Petr Plodík <plodik@outlook.com>
2020-01-20 21:26:07 +0100

    Added selenium testing

---
+++ AutomatedUITests/AutomatedUITests.cs
@@ -0,0 +1,34 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace AutomatedUITests
+{
+    public class AutomatedUITests : IDisposable
+    {
+        private readonly IWebDriver _driver;
+        public AutomatedUITests()
+        {
+            _driver = new ChromeDriver();
+        }
+
+        public void Dispose()
+        {
+            _driver.Quit();
+            _driver.Dispose();
+        }
+
+        [Fact]
+        public void Create_WhenExecuted_ReturnsCreateView()
+        {
+            _driver.Navigate()
+                .GoToUrl("https://localhost:44311/Home/Privacy");
+
+            Assert.Equal("Privacy Policy - ASPNETCoreTest1", _driver.Title);
+            Assert.Contains("Use this page to detail your site's privacy policy", _driver.PageSource);
+        }
+    }
+}
