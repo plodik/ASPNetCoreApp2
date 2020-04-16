ca93d863d7b2f97e9dd4d6259b1a6d0bed515085
Petr Plodík <plodik@outlook.com>
2020-04-16 10:28:29 +0200

    Dalsi nova testovaci metoda

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -28,5 +28,11 @@ namespace UnitTestProject1
         {
             Assert.AreEqual(2, 2);
         }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            Assert.AreEqual(3, 3);
+        }
     }
 }
