dbd50b78de9747002439ead5b267cbbe519f5aa1
Petr Plodík <plodik@outlook.com>
2020-04-16 08:22:34 +0200

    Pridany unit testy pro Calculator + objekt Calculator

---
+++ ASPNETCoreTest1/Calculator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNETCoreTest1
+{
+    public class Calculator
+    {
+        public int Sum(int input1, int input2)
+        {
+            if(input1 > 5)
+            {
+                return input1 + input2 + 0;
+            }
+            else
+            {
+                return input1 + input2;
+            }
+        }
+    }
+}
---
+++ UnitTestProject1/CalculatorTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ASPNETCoreTest1;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ASPNETCoreTest1.Tests
+{
+    [TestClass()]
+    public class CalculatorTests
+    {
+        [TestMethod()]
+        public void SumTest()
+        {
+            Calculator calc = new Calculator();
+            int result = calc.Sum(1, 3); // result should be 4
+            Assert.AreEqual(4, result);
+
+            result = calc.Sum(10, 3); // result should be 13
+            Assert.AreEqual(13, result);
+        }
+    }
+}
